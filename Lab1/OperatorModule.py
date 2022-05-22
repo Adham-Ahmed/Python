50a2e937b00be968197cd2c0ac825daa7fe16cc9
Adham <adhamahmed1234321@gmail.com>
2022-05-22 23:40:07 +0200

    added minor missing part

---
+++ Lab1/OperatorModule.py
@@ -0,0 +1,9 @@
+import operator
+
+a=10;b=20
+print(operator.le(a, b))
+print(operator.eq(a, b))
+print(operator.ne(a, b))
+print(operator.ge(a, b))
+print(operator.lt(a, b))
+print(operator.gt(a, b))
