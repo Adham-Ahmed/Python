f7409ec37760f65eb36506c8702ad2ce6e293c4b
Adham <adhamahmed1234321@gmail.com>
2022-05-22 20:24:36 +0200

    problem 3 done

--- Problem3.py
+++ Problem3.py
@@ -1,6 +1,17 @@
 # Problem 3
 
+def front_back(a, b):
+    print (a, b)
+    a_indx = int((len(a)+1)/2)
+    b_indx = int((len(b)+1)/2)
 
+    print (a[:a_indx] + b[:b_indx] + a[a_indx:] +b[b_indx:])
+    print ("\n")
+
+front_back("ab", "cd")
+front_back("abc", "de")
+front_back("ab", "cde")
+front_back("abc", "def")
 
 
 
