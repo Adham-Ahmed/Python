c181c4faef92e728115414d186ce8a9c602e2026
Adham <adhamahmed1234321@gmail.com>
2022-05-22 20:48:28 +0200

    problem 6 done

--- Problem6.py
+++ Problem6.py
@@ -1,1 +1,19 @@
-#Problem6
+# #Problem6
+
+# The program takes a string and a character and returns a list with all the
+# locations that character was found in the given string.
+# Implementation hint:
+# String “Google” char ‘o’
+# Output: [1,2]
+
+def indexFinder(inputString,inputChar):
+
+    lst = []
+    for pos,char in enumerate(inputString):
+        if(char == inputChar):
+            lst.append(pos)
+    print(lst)
+
+
+indexFinder("adham","a")
+indexFinder("Google","o")
