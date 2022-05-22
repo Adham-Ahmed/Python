55dd0538431980a98ca2e080f0ae01068a37b399
Adham <adhamahmed1234321@gmail.com>
2022-05-22 20:33:48 +0200

    problem 5 done

--- Problem5.py
+++ Problem5.py
@@ -1,3 +1,16 @@
 # Problem 5
+import sys
 
+# The program takes a string and remove the vowels character from it then
+# print its new version
+# Implementation hint:
+# So, “Mobile” becomes “Mbl”
 
+inputString = sys.argv[1]
+vowels=['a','e','o','u','i']
+
+for i in inputString:
+    if(i in vowels):
+        inputString=inputString.replace(i,"")
+
+print(inputString)
